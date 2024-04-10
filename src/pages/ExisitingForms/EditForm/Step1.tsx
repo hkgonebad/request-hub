5f9ce205fe3e00a722fd033f77a93f44a8f746cc
hkgonebad <harry007007@gmail.com>
2024-04-10 20:08:59 +0530

    start: Step Wizard component forms

--- src/pages/ExisitingForms/EditForm/Step1.tsx
+++ src/pages/ExisitingForms/EditForm/Step1.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import { Accordion, Form, Stack, ToggleButton } from "react-bootstrap";
+import { FiEdit2, FiMove, FiTrash2 } from "react-icons/fi";
 import { useWizard } from "react-use-wizard";
+import OtpInput from "react-otp-input";
 
 const Step1 = () => {
-  const { handleStep, previousStep, nextStep } = useWizard();
+  const { isLoading, isLastStep, isFirstStep, activeStep, previousStep, nextStep, handleStep } = useWizard();
+
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState("1");
+  const [otp, setOtp] = useState("");
 
   // Attach an optional handler
   handleStep(() => {
@@ -9,10 +17,111 @@ const Step1 = () => {
   });
 
   return (
-    <>
-      <button onClick={() => previousStep()}>Previous ⏮️</button>
-      <button onClick={() => nextStep()}>Next ⏭</button>
-    </>
+    <div className="wzStep">
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Mandatory Fields for User Validation </Accordion.Header>
+          <Accordion.Body>
+            {/* 1 */}
+            <Stack className="wzItem" direction="horizontal" gap={2}>
+              <FiMove className="icon dragIcon" />
+              <span>1</span>
+              <Form.Group className="d-flex align-items-center gap-2" controlId="wzForm1">
+                <label htmlFor="">Select User</label>
+                <input type="radio" className="btn-check" name="options" id="option1" />
+                <label className="btn btn-secondary" htmlFor="option1">
+                  Customer
+                </label>
+                <input type="radio" className="btn-check" name="options" id="option2" />
+                <label className="btn btn-secondary" htmlFor="option2">
+                  Employee
+                </label>
+              </Form.Group>
+
+              {/*  */}
+              <Stack className="actionBtns ms-auto" direction="horizontal" gap={2}>
+                <a href="#!" className="btn btnEdit text-success">
+                  <FiEdit2 />
+                </a>
+                <a href="#!" className="btn btnDelete text-danger">
+                  <FiTrash2 />
+                </a>
+              </Stack>
+            </Stack>
+
+            {/* 2 */}
+            <Stack className="wzItem" direction="horizontal" gap={2}>
+              <FiMove className="icon dragIcon" />
+              <span>2</span>
+              <Form.Group className="d-flex align-items-center gap-2" controlId="wzForm1">
+                <Form.Control placeholder="Enter Mobile Number" />
+              </Form.Group>
+
+              {/*  */}
+              <Stack className="actionBtns ms-auto" direction="horizontal" gap={2}>
+                <a href="#!" className="btn btnEdit text-success">
+                  <FiEdit2 />
+                </a>
+                <a href="#!" className="btn btnDelete text-danger">
+                  <FiTrash2 />
+                </a>
+              </Stack>
+            </Stack>
+
+            {/* 3 */}
+            <Stack className="wzItem" direction="horizontal" gap={2}>
+              <FiMove className="icon dragIcon" />
+              <span>4</span>
+              <Form.Group className="d-flex align-items-center gap-2" controlId="wzForm3">
+                <Form.Control placeholder="Enter Email Id" />
+              </Form.Group>
+
+              {/*  */}
+              <Stack className="actionBtns ms-auto" direction="horizontal" gap={2}>
+                <a href="#!" className="btn btnEdit text-success">
+                  <FiEdit2 />
+                </a>
+                <a href="#!" className="btn btnDelete text-danger">
+                  <FiTrash2 />
+                </a>
+              </Stack>
+            </Stack>
+
+            {/* 4 */}
+            <Stack className="wzItem" direction="horizontal" gap={2}>
+              <FiMove className="icon dragIcon" />
+              <span>4</span>
+              <Form.Group className="d-flex align-items-center gap-2" controlId="wzForm3">
+                <label htmlFor="">OTP Validation</label>
+                <OtpInput
+                  value={otp}
+                  onChange={setOtp}
+                  numInputs={6}
+                  skipDefaultStyles={true}
+                  containerStyle="otpInput"
+                  inputType="number"
+                  renderInput={(props) => <input {...props} className="form-control" />}
+                />
+              </Form.Group>
+
+              {/*  */}
+              <Stack className="actionBtns ms-auto" direction="horizontal" gap={2}>
+                <a href="#!" className="btn btnEdit text-success">
+                  <FiEdit2 />
+                </a>
+                <a href="#!" className="btn btnDelete text-danger">
+                  <FiTrash2 />
+                </a>
+              </Stack>
+            </Stack>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Form Fields</Accordion.Header>
+          <Accordion.Body>Lorem ipsum dolor sit amet</Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
   );
 };
 
--- src/pages/ExisitingForms/EditForm/index.tsx
+++ src/pages/ExisitingForms/EditForm/index.tsx
@@ -1,9 +1,10 @@
 import { SubBusinessUnit } from "@/models/interfaces";
 
 import { Stack } from "react-bootstrap";
-import { FiEdit2, FiTrash2 } from "react-icons/fi";
+import { FiEdit2, FiEye, FiTrash2 } from "react-icons/fi";
 
-import { Wizard } from "react-use-wizard";
+import { useParams } from "react-router-dom";
+import { Wizard, useWizard } from "react-use-wizard";
 import { motion, AnimatePresence } from "framer-motion";
 
 import Step1 from "./Step1";
@@ -11,10 +12,79 @@ import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
 
-const EditForm = ({ item }: { item: SubBusinessUnit }) => {
-  console.log(item);
+// Wizard Navigation Menu items
+const navItems = [
+  { label: "Webform Configuration", id: 0 },
+  { label: "Styling / Branding", id: 1 },
+  { label: "Customer Notifications", id: 2 },
+  { label: "Preview Form", id: 3 },
+  { label: "Raise Request", id: 4 },
+];
 
-  const Wrapper = () => <AnimatePresence mode="wait" />;
+const EditForm = ({ item }: { item: SubBusinessUnit }) => {
+  const { id } = useParams<{ id: string }>();
+  console.log(item, id);
+
+  // const Wrapper = () => <AnimatePresence exitBeforeEnter />;
+
+  //
+  const WizardNav = () => {
+    const { activeStep, goToStep } = useWizard();
+    return (
+      <div className="wizardNav">
+        <Stack className="nav" direction="horizontal">
+          {navItems.map((item, index) => (
+            <button
+              key={index}
+              className={`${activeStep === index ? "active" : ""} btn`}
+              onClick={() => {
+                goToStep(index);
+              }}
+            >
+              {item.label}
+            </button>
+          ))}
+        </Stack>
+        <Stack className="wizardHeader" direction="horizontal">
+          <div className="wzImg me-auto">
+            <img src={item.img} alt="" />
+          </div>
+          <Stack className="wzMeta" direction="horizontal">
+            <b>
+              Last Update: <span>22/03/2019</span>
+            </b>
+            <b>
+              By Admin Config: <span>Amit Ray</span>
+            </b>
+            <b>
+              Business Approval By: <span>Shailesh Naik</span>
+            </b>
+          </Stack>
+        </Stack>
+      </div>
+    );
+  };
+
+  // Wizard Footer
+  const WizardFooter = () => {
+    const { nextStep, previousStep, isLoading, activeStep, stepCount, isLastStep, isFirstStep } = useWizard();
+    return (
+      <div className="wizardFooter">
+        <Stack className="btns" direction="horizontal">
+          <a href="btn btn-link ">
+            <FiEye /> Preview
+          </a>
+          <button className="btn btn-outline-light" onClick={() => previousStep()} disabled={isFirstStep || isLoading}>
+            Cancel
+          </button>
+          <button className="btn btn-outline-light">Save Draft</button>
+          <button className="btn btn-primary" onClick={() => nextStep()} disabled={isLastStep || isLoading}>
+            {isLastStep ? "Finish" : "Next"}
+          </button>
+        </Stack>
+      </div>
+    );
+  };
 
   return (
     <section className="block stepsWizard">
@@ -31,7 +101,8 @@ const EditForm = ({ item }: { item: SubBusinessUnit }) => {
           </button>
         </Stack>
 
-        <Wizard startIndex={0} wrapper={<Wrapper />}>
+        {/* Wizard */}
+        <Wizard startIndex={0} header={<WizardNav />} footer={<WizardFooter />}>
           <Step1 />
           <Step2 />
           <Step3 />
