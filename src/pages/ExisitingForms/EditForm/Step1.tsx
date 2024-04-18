5f3297c18559cec98ac69bbdd73ebdf5d23f8fd0
hkgonebad <harry007007@gmail.com>
2024-04-18 11:42:09 +0530

    refactor: add 'add new field ' modal

--- src/pages/ExisitingForms/EditForm/Step1.tsx
+++ src/pages/ExisitingForms/EditForm/Step1.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Accordion, Form, InputGroup, Stack } from "react-bootstrap";
+import { Accordion, Button, Form, InputGroup, Modal, Stack } from "react-bootstrap";
 import { FiEdit, FiMove, FiTrash2 } from "react-icons/fi";
 import { useWizard } from "react-use-wizard";
 import OtpInput from "react-otp-input";
@@ -7,11 +7,16 @@ import OtpInput from "react-otp-input";
 const Step1 = () => {
   const { handleStep } = useWizard();
   const [otp, setOtp] = useState("");
+  const [show, setShow] = useState(false);
 
   // Attach an optional handler
   handleStep(() => {
     alert("Going to step 2");
   });
+
+  // Modal
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div className="wzStep">
@@ -156,7 +161,9 @@ const Step1 = () => {
 
               {/*  */}
               <Stack className="actionBtns ms-auto" direction="horizontal" gap={2}>
-                <button className="btn btn-primary">Add New Field</button>
+                <button className="btn btn-primary" onClick={handleShow}>
+                  Add New Field
+                </button>
               </Stack>
             </Stack>
           </Accordion.Body>
@@ -166,6 +173,64 @@ const Step1 = () => {
           <Accordion.Body>Lorem ipsum dolor sit amet</Accordion.Body>
         </Accordion.Item>
       </Accordion>
+
+      {/* Modal */}
+      <Modal show={show} onHide={handleClose} centered size="lg" className="formModal">
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Field</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="addField">
+            <Form.Group className="mb-3" controlId="formBasicList">
+              <Form.Label>Predefined List</Form.Label>
+              <Form.Select aria-label="Default select example">
+                <option>Select Predefined List</option>
+                <option value="1">Customer</option>
+                <option value="2">Employee</option>
+              </Form.Select>
+            </Form.Group>
+            <div className="text-center">
+              <h5>Or Create New (Customize)</h5>
+            </div>
+
+            <div className="row">
+              <div className="col-md-4">
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                  <Form.Label>Field Title</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Field Title" />
+                </Form.Group>
+              </div>
+              <div className="col-md-4">
+                <Form.Group className="mb-3">
+                  <Form.Label>Field Title</Form.Label>
+                  <br />
+                  <Form.Check type="radio" inline label="Optional" name="addfield" id="afo" />
+                  <Form.Check type="radio" inline label="Mandatory" name="addfield" id="afm" />
+                </Form.Group>
+              </div>
+              <div className="col-md-4">
+                <Form.Group className="mb-3" controlId="formBasicType">
+                  <Form.Label>Input Type</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Dropdown Checklist</option>
+                    <option value="1">Text</option>
+                    <option value="2">Number</option>
+                    <option value="3">Email</option>
+                  </Form.Select>
+                </Form.Group>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-outline-danger" onClick={handleClose}>
+            Cancel
+          </button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
