c0f0627e800a299ef9309e221448ee2a4870e7bc
hkgonebad <harry007007@gmail.com>
2025-04-13 14:56:45 +0530

    add: new logo assets for Avengers, Defenders, Fantastic Four, Guardians of the Galaxy, and X-Men; update image references in business units data

--- src/pages/ExisitingForms/businessUnitsData.ts
+++ src/pages/ExisitingForms/businessUnitsData.ts
@@ -5,64 +5,64 @@ const businessUnits: BusinessUnit[] = [
   {
     id: 0,
     name: "Avengers Division",
-    img: "/src/assets/img/logos/avengers.svg",
+    img: "/src/assets/img/logos/avengers.png",
     subBusinessUnits: [
       {
         id: 1,
         name: "Core Team",
-        img: "/src/assets/img/logos/core-team.svg",
+        img: "/src/assets/img/logos/avengers.png",
       },
       {
         id: 2,
         name: "New Avengers",
-        img: "/src/assets/img/logos/new-avengers.svg",
+        img: "/src/assets/img/logos/avengers.png",
       },
       {
         id: 3,
         name: "Young Avengers",
-        img: "/src/assets/img/logos/young-avengers.svg",
+        img: "/src/assets/img/logos/avengers.png",
       },
       {
         id: 4,
         name: "West Coast",
-        img: "/src/assets/img/logos/west-coast.svg",
+        img: "/src/assets/img/logos/avengers.png",
       },
       {
         id: 5,
         name: "Secret Avengers",
-        img: "/src/assets/img/logos/secret-avengers.svg",
+        img: "/src/assets/img/logos/avengers.png",
       },
       {
         id: 6,
         name: "Mighty Avengers",
-        img: "/src/assets/img/logos/mighty-avengers.svg",
+        img: "/src/assets/img/logos/avengers.png",
       },
       {
         id: 7,
         name: "Dark Avengers",
-        img: "/src/assets/img/logos/dark-avengers.svg",
+        img: "/src/assets/img/logos/avengers.png",
       },
     ],
   },
   {
     id: 8,
     name: "X-Men Division",
-    img: "src/assets/img/logos/x-men.svg",
+    img: "src/assets/img/logos/xmen.jpg",
   },
   {
     id: 9,
     name: "Fantastic Four",
-    img: "src/assets/img/logos/fantastic-four.svg",
+    img: "src/assets/img/logos/fantastic4.jpg",
   },
   {
     id: 10,
     name: "Defenders",
-    img: "src/assets/img/logos/defenders.svg",
+    img: "src/assets/img/logos/defenders.png",
   },
   {
     id: 11,
     name: "Guardians of the Galaxy",
-    img: "src/assets/img/logos/guardians.svg",
+    img: "src/assets/img/logos/guardiansofgalaxy.png",
   },
 ];
 
